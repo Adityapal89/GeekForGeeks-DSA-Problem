6c8990c95979b1d0514fe618560ae57b2c9712ff
Aditya pal <adityapal622820@gmail.com>
2026-02-18 17:22:20 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count of distinct substrings/count-of-distinct-substrings.java
@@ -0,0 +1,21 @@
+class Solution {
+    static class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+    }
+    public static int countSubs(String s) {
+        TrieNode root = new TrieNode();
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            TrieNode node = root;
+            for (int j = i; j < s.length(); j++) {
+                int idx = s.charAt(j) - 'a';
+                if (node.children[idx] == null) { //if node not null then insert node
+                    node.children[idx] = new TrieNode();
+                    count++;
+                }
+                node = node.children[idx];//updata index
+            }
+        }
+        return count;
+    }
+}
