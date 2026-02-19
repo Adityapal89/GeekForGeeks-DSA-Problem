93dd12f8babd46e9d146966209159252df28bc0c
Aditya pal <adityapal622820@gmail.com>
2026-02-19 18:48:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Merge and Sort/merge-and-sort.java
+++ Difficulty: Basic/Merge and Sort/merge-and-sort.java
@@ -2,18 +2,12 @@ class Solution {
     public ArrayList<Integer> mergeNsort(int[] arr1, int[] arr2) {
         
         TreeSet<Integer> set = new TreeSet<>();
-        
-        // Add elements of first array
         for (int x : arr1) {
             set.add(x);
         }
-        
-        // Add elements of second array
         for (int x : arr2) {
             set.add(x);
         }
-        
-        // Convert set to ArrayList and return
         return new ArrayList<>(set);
     }
 }
