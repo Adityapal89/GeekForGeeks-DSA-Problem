f14b220c4d0fc6bc82588043a2c72bede4935d17
Aditya pal <adityapal622820@gmail.com>
2026-03-04 07:51:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Topological Sort/topological-sort.java
@@ -0,0 +1,35 @@
+class Solution {
+    public ArrayList<Integer> topoSort(int V, int[][] edges) {
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < V; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for (int i = 0; i < edges.length; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            adj.get(u).add(v);
+        }
+        boolean[] visited = new boolean[V];
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                dfs(i, adj, visited, st);
+            }
+        }
+        ArrayList<Integer> topo = new ArrayList<>();
+        while (!st.isEmpty()) {
+            topo.add(st.pop());
+        }
+        return topo;
+    }
+
+    private void dfs(int node, ArrayList<ArrayList<Integer>> adj, boolean[] visited, Stack<Integer> st) {
+        visited[node] = true;
+        for (int neighbour : adj.get(node)) {
+            if (!visited[neighbour]) {
+                dfs(neighbour, adj, visited, st);
+            }
+        }
+        st.push(node);
+    }
+}
