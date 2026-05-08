b1c5f7a18d417f76a15d9241dec306aa07e9d491
Aditya pal <adityapal622820@gmail.com>
2026-05-09 02:36:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Linked List End Insertion/linked-list-end-insertion.java
@@ -0,0 +1,26 @@
+/*
+class Node{
+    int data;
+    Node next;
+
+    Node(int x){
+        data = x;
+        next = null;
+    }
+}
+*/
+class Solution {
+    public Node insertAtEnd(Node head, int x) {
+        // code here
+        Node ad = new Node(x);
+        if(head == null){
+            return ad;
+        }
+        Node temp = head;
+        while(temp.next != null){
+            temp = temp.next;
+        }
+        temp.next = ad;
+        return head;
+    }
+}
