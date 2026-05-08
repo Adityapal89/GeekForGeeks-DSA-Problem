c508622ab67ecdcd49da0c9c00e1a0aa28faf5e3
Aditya pal <adityapal622820@gmail.com>
2026-05-09 02:20:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Length of Linked List/length-of-linked-list.java
@@ -0,0 +1,20 @@
+/*
+Definition for Linked List
+class Node{
+    int data;
+    Node next;
+    Node(int a){  data = a; next = null; }
+}
+*/
+class Solution {
+    public int getCount(Node head) {
+        // code here
+        Node temp = head;
+        int count = 0;
+        while(temp != null){
+            count++;
+            temp = temp.next; 
+        }
+        return count;
+    }
+}
