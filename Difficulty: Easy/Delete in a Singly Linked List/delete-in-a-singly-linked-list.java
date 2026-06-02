5e2b97cc05ba813fb7e404de612305c2341ac9c6
Aditya pal <adityapal622820@gmail.com>
2026-06-02 15:45:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Delete in a Singly Linked List/delete-in-a-singly-linked-list.java
@@ -0,0 +1,29 @@
+/*
+class Node
+{
+    int data;
+    Node next;
+
+    Node(int d)
+    {
+        this.data = d;
+        this.next = null;
+    }
+}
+*/
+class Solution {
+    Node deleteNode(Node head, int x) {
+        // code here
+        Node temp = head;
+        if(head == null) return null;
+        if(x == 1) return head.next;
+        for(int i=1; i<x-1; i++){
+            temp = temp.next;
+        }
+        if(temp.next != null){
+            temp.next = temp.next.next;
+        }
+        return head;
+        
+    }
+}
