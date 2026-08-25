1940051ac19a795202380c3c367adf45eb5673d6
Aditya pal <adityapal622820@gmail.com>
2026-08-25 15:02:54 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/1-D prefix sum/1d-prefix-sum.java
@@ -0,0 +1,12 @@
+class Solution {
+    public ArrayList<Integer> prefSum(int[] arr) {
+        // code here
+        ArrayList<Integer> al = new ArrayList<>();
+        int sum = 0;
+        for(int i=0; i<arr.length; i++){
+            sum += arr[i];
+            al.add(sum);
+        }
+        return al;
+    }
+}
