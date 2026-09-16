bce56a34b13f5ddfd4101c1cba96171a566382a1
Aditya pal <adityapal622820@gmail.com>
2026-09-16 18:25:21 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Square Root/square-root.java
+++ Difficulty: Easy/Square Root/square-root.java
@@ -1,22 +1,11 @@
 class Solution {
     int floorSqrt(int n) {
         // code here
-        int low = 1, high = n, ans = 0;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-
-            if (mid * mid == n) {
-                return mid;   // perfect square
-            }
-            else if (mid * mid < n) {
-                ans = mid;    // possible answer
-                low = mid + 1;
-            }
-            else {
-                high = mid - 1;
-            }
+        int root = 0;
+        for(int i=0; i<=n; i++){
+            if(i*i > n) break;
+            root = i;
         }
-        return ans;
+        return root;
     }
 }
